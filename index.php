9aeb61338bdc58f526d89a0e3b8410d8c231d292
BiggyDev <vincent.carpentier111@gmail.com>
2018-11-12 13:08:12 +0100

    biggy

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 include('inc/pdo.php');
 include('inc/functions.php');
-$title = 'ACCUEIL';
+$title = 'Accueil';
 include('inc/header.php');?>
 
     <section class="leftcolumn">
-        <p>Pourquoi le carnet de vaccination electronique ?</p>
+        <p>Pourquoi le carnet de vaccination électronique ?</p>
         <ul>
             <li>Simple, pratique et gratuit</li>
             <li>Centralisez les vaccins de toute la famille</li>
@@ -18,16 +18,7 @@ include('inc/header.php');?>
 
         </div>
     </div>
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-<?php include('inc/footer.php'); 
-=======
->>>>>>> 1dbcfaee2168038084a3372f685fe0359c5d6cd1
+
+
 <?php include('inc/footer.php');
-
-
-
-
->>>>>>> 87fe06c15c6690562c7501843a12234b6e28dea0
