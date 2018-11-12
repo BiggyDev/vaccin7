2964e3bd6d267ad02b2a4ea47165d474399ed9ce
Yamna Melki <melkiyamna@protonmail.com>
2018-11-12 13:00:39 +0100

    yamna

--- index.php
+++ index.php
@@ -4,10 +4,20 @@ include('inc/functions.php');
 $title = 'ACCUEIL';
 include('inc/header.php');?>
 
-    <div id="content">
+    <section class="leftcolumn">
+        <p>Pourquoi le carnet de vaccination electronique ?</p>
+        <ul>
+            <li>Simple, pratique et gratuit</li>
+            <li>Centralisez les vaccins de toute la famille</li>
+            <li>Confidentiel et sécurisé</li>
+            <li>Independant des firmes pharmaceutiques</li>
+        </ul>
+    </section>
+    <div class="wrap">
+        <div id="content">
 
+        </div>
     </div>
-
 <?php include('inc/footer.php');
 
 
