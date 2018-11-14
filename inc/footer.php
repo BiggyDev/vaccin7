cf18853fbf40f7e3cf1f732efe80c6f1ca9b4a8c
unknown <jeanluc.oudjoudi@gmail.com>
2018-11-14 13:38:29 +0100

    jeanluc

--- inc/footer.php
+++ inc/footer.php
@@ -6,18 +6,15 @@
           <div class="col-md-6">
             <h5 class="text-uppercase">NOUS CONTACTER</h5>
               <div>
-                <i class="material-icons">room</i>
-                <span>24 Place Saint-Marc 76000 Rouen</span>
+              <span><i class="material-icons">room</i> 24 Place Saint-Marc 76000 Rouen</span>
               </div>
 
               <div class="telephone">
-                <i class="material-icons">call</i>
-                <span>02 32 10 25 01</span>
-
+                <span><i class="material-icons">call</i> 02 32 10 25 01</span>
               </div>
+
               <div class="mail">
-                <i class="material-icons">email</i>
-                <span><a href="mailto:vactualise@gmail.com">vactualise@gmail.com</a></span>
+                <span><i class="material-icons">email</i><a href="mailto:vactualise@gmail.com">vactualise@gmail.com</a></span>
               </div>
           </div>
 
@@ -27,11 +24,7 @@
             <ul class="list-unstyled">
               <li><a href="#">Mentions légales</a></li>
               <li><a href="FAQ.php">FAQ</a></li>
-
               <li><a href="#haut">Retour en haut de la page</a></li>
-
-              <li><a href="#">Plan du site</a></li>
-
             </ul>
 
           </div>
@@ -41,8 +34,8 @@
             <h5 class="text-uppercase">Réseaux Sociaux</h5>
 
             <ul class="list-unstyled">
-                <li><a target="_blank" href="https://fr-fr.facebook.com/"><img src="asset/img/facebookcarre.png" alt="facebook"></a></li>
-                <li><a target="_blank" href="https://twitter.com/?lang=fr"><img src="asset/img/twittercarre.png" alt="twitter"></a></li>
+                <li class="fb"><a target="_blank" href="https://fr-fr.facebook.com/"><img src="asset/img/facebookcarre.png" alt="facebook"></a></li>
+                <li class="twt"><a target="_blank" href="https://twitter.com/?lang=fr"><img src="asset/img/twittercarre.png" alt="twitter"></a></li>
 
             </ul>
 
