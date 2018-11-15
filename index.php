a60d9a47d60a4b7867bb47f0778665a3efaf5977
Yamna Melki <melkiyamna@protonmail.com>
2018-11-15 14:19:51 +0100

    yamna

--- index.php
+++ index.php
@@ -15,13 +15,13 @@ include('inc/header.php');?>
             <ul class="slides">
 
               <li>
-                    <img src="asset/img/grippe.jpg" />
+                    <img src="asset/img/slider1.svg" />
                 </li>
                 <li>
-                    <img src="asset/img/assurance-maladie.jpg" />
+                    <img src="asset/img/slider2.svg" />
                 </li>
                 <li>
-                    <img src="asset/img/oms.png" />
+                    <img src="asset/img/slider3.svg" />
                 </li>
             </ul>
         </div>
@@ -34,21 +34,11 @@ include('inc/header.php');?>
             <section class="section2">
               <div class="presentation">
                 <h5>Recommandé par</h5>
-              <p>Pourquoi le carnet de vaccination électronique ?</p>
-                <ul>
-                    <li>Simple, pratique et gratuit</li>
-                    <li>Centralisez les vaccins de toute la famille</li>
-                    <li>Confidentiel et sécurisé</li>
-
-                    <li>Independant des firmes Pharmaceutiques</li>
-                </ul>
 
                 <object type="" data="https://www.santemagazine.fr/actualites">
 
                 </object>
-<!--                <a href="http://www.vaccinesafetynet.org/#gsc.tab=0" target="_blank">-->
-<!--                    <img src="asset/img/vaccine_safety_net.png" alt="Vaccine Safety Net Member">-->
-<!--                </a>-->
+
                 </div>
 
                 <div class="twitteractu">
