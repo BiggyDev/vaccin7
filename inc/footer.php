9c086c205cdcbcb55906ccd9cdca11e85aa6c4e9
BiggyDev <vincent.carpentier111@gmail.com>
2018-11-15 17:04:17 +0100

    biggy

--- inc/footer.php
+++ inc/footer.php
@@ -44,15 +44,20 @@
 <script
   src="https://code.jquery.com/jquery-2.2.4.min.js"
   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-  crossorigin="anonymous"></script><script src="asset/slicknav/jquery.slicknav.min.js"></script>
+  crossorigin="anonymous"></script>
+<script src="asset/slicknav/jquery.slicknav.min.js"></script>
 <script src="asset/flexslider/jquery.flexslider.js"></script>
 <script>
     $(function(){
         $('.menu').slicknav();
     });
     $(window).load(function() {
-        $('.flexslider').flexslider();
+        $('.flexslider').flexslider({
+        animation: "fade", //slide
+        controlNav: false,
+        directionNav: false
     });
+  });
 </script>
 
 </body>
